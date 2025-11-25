0000d71a619d00fe2895544645ee3a57cbd39645
insiderIns999 <152618604+insiderIns999@users.noreply.github.com>
2025-11-25 09:20:35 +0300

    Add files via upload

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,1 +1,59 @@
+import { useState } from "react";
 
+const Header = () => {
+  const [visibility, setVisibility] = useState(false);
+
+  const changeVisibility = () => {
+    if (visibility == false) setVisibility(true);
+    else setVisibility(false);
+  };
+
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__block">
+          <div className="header__logo _show _light">
+            <a href="" target="_self">
+              <img src="images/logo.png" alt="logo" />
+            </a>
+          </div>
+          <div className="header__logo _dark">
+            <a href="" target="_self">
+              <img src="images/logo_dark.png" alt="logo" />
+            </a>
+          </div>
+          <nav className="header__nav">
+            <button className="header__btn-main-new _hover01" id="btnMainNew">
+              <a href="#popNewCard">Создать новую задачу</a>
+            </button>
+            <a
+              //href="#user-set-target"
+              className="header__user _hover02"
+              onClick={changeVisibility}
+            >
+              Ivan Ivanov
+            </a>
+            {visibility && (
+              <div
+                className="{`header__pop-user-set pop-user-set ${visibility ? '' : 'change-display'}`}"
+                id="user-set-target"
+              >
+                <p className="pop-user-set__name">Ivan Ivanov</p>
+                <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
+                <div className="pop-user-set__theme">
+                  <p>Темная тема</p>
+                  <input type="checkbox" className="checkbox" name="checkbox" />
+                </div>
+                <button type="button" className="_hover03">
+                  <a href="#popExit">Выйти</a>
+                </button>
+              </div>
+            )}
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
