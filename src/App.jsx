845f1e04c7b66ae0480dde15d1818e532f063497
insiderIns999 <152618604+insiderIns999@users.noreply.github.com>
2025-11-26 11:59:33 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import Wrapper from "./components/Wrapper";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  document.writeln(
+    "<span style='display: table; margin: 20% auto 0; font-size: 200%;'>Приложение загружается...</span>"
+  );
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <>
-      <Wrapper />
+      <Wrapper loading={loading} />
       <script src="js/script.js"></script>
     </>
   );
