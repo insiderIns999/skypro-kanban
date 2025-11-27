482581e002c35d0f31dff2be763991e342b38d15
insiderIns999 <152618604+insiderIns999@users.noreply.github.com>
2025-11-27 12:24:56 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,22 @@ import Wrapper from "./components/Wrapper";
 function App() {
   const [loading, setLoading] = useState(true);
 
-  document.writeln(
-    "<span style='display: table; margin: 20% auto 0; font-size: 200%;'>Приложение загружается...</span>"
-  );
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       setLoading(false);
     }, 3000);
+    return () => clearTimeout(timer);
   }, []);
 
   return (
     <>
+      {loading && (
+        <span
+          style={{ display: "table", margin: "20% auto 0", fontSize: "200%" }}
+        >
+          Приложение загружается...
+        </span>
+      )}
       <Wrapper loading={loading} />
       <script src="js/script.js"></script>
     </>
