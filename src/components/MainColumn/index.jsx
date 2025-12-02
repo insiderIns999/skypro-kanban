ced7ad2caa22bc6149c02588b8fb69fed5ef33a6
insiderIns999 <152618604+insiderIns999@users.noreply.github.com>
2025-12-02 14:56:04 +0300

    Add files via upload

--- src/components/MainColumn/index.jsx
+++ src/components/MainColumn/index.jsx
@@ -2,14 +2,16 @@ import CardsItem from "../CardsItem";
 
 const MainColumn = () => {
   return (
-    <div className="main__column column">
-      <div className="column__title">
-        <p>Без статуса</p>
+    <>
+      <div className="main__column column">
+        <div className="column__title">
+          <p>Без статуса</p>
+        </div>
+        <div className="cards">
+          <CardsItem />
+        </div>
       </div>
-      <div className="cards">
-        <CardsItem />
-      </div>
-    </div>
+    </>
   );
 };
 
