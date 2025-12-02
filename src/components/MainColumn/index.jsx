46b1a2c7c89c955e9cbe69c69a0769d5260f97c8
insiderIns999 <152618604+insiderIns999@users.noreply.github.com>
2025-12-02 10:37:32 +0300

    Add files via upload

--- src/components/MainColumn/index.jsx
+++ src/components/MainColumn/index.jsx
@@ -1,1 +1,16 @@
+import { CardsItem } from "../CardsItem";
 
+const MainColumn = () => {
+  return (
+    <div className="main__column column">
+      <div className="column__title">
+        <p>Без статуса</p>
+      </div>
+      <div className="cards">
+        <CardsItem />
+      </div>
+    </div>
+  );
+};
+
+export default MainColumn;
